e6bcc8a3e7702ab7058f8145a98cba83fed69502
pybdrew <116611820+pybdrew@users.noreply.github.com>
2025-12-06 16:20:49 -0600

    Logging

---
+++ pokestash/src/main/java/com/poke/stash/config/LoggingInterceptor.java
@@ -0,0 +1,34 @@
+package com.poke.stash.config;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class LoggingInterceptor implements HandlerInterceptor
+{
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception
+    {
+        logger.info("Entering: {} {}", request.getMethod(), request.getRequestURI());
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception
+    {
+        logger.info("Exiting: {} {}", request.getMethod(), request.getRequestURI());
+
+        if (ex != null)
+        {
+            logger.error("Exception occurred:", ex);
+        }
+    }
+}
